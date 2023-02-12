776dae2f7c713e0a794fe8e3aea67d9b865e5268
Alma Godinez <almagodinezg@gmail.com>
2023-02-11 20:04:32 -0600

    first commit

---
+++ CalculadoraEstaciones.js
@@ -0,0 +1,20 @@
+let mes = prompt("ingrese un mes en numero tal sea como enero:1, febrero: 2 y sucesivamente");
+
+if(mes == 12 ||  mes == 1 || mes == 2) {
+  alert("es invierno")
+  alert("Alma Ester Godinez Godinez 22011401")
+}
+else if(mes == 3 || mes == 4 || mes == 5) {
+  alert("es primavera")
+  alert("Alma Ester Godinez Godinez 22011401")
+}
+else if(mes == 6 || mes == 7 || mes == 8) {
+  alert("es verano")
+  alert("Alma Ester Godinez Godinez 22011401")
+}
+else if(mes == 9 || mes == 10 || mes == 11){
+ alert("es otoÃƒÂ±o")
+ alert("Alma Ester Godinez Godinez 22011401")
+}
+else{ alert("mes invalido")
+}
